e885a24fd36cd114cb459126010c9cd3263c30f4
vegatality <vegatality@gmail.com>
2023-05-01 15:02:11 +0900

    devtool deactivate on deploy

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -6,7 +6,11 @@ const store = configureStore({
     reducer: {
         todo,
     },
-    enhancers: [composeWithDevTools()],
+    enhancers: composeWithDevTools({
+        // 개발 환경에서는 항상 DevTools를 활성화
+        // 배포 환경에서는 process.env.NODE_ENV가 'production'인 경우에만 활성화
+        shouldCompose: process.env.NODE_ENV !== "production",
+    }),
 });
 
 export default store;
