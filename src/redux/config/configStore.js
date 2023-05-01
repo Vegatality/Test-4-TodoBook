fa84d08410070d3d8cb16a16a1dd34df6a0b92b6
vegatality <vegatality@gmail.com>
2023-05-01 14:33:25 +0900

    backup before deploy

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
 import todo from "redux/modules/todoSlice";
 
 const store = configureStore({
     reducer: {
         todo,
     },
+    enhancers: [composeWithDevTools()],
 });
 
 export default store;
